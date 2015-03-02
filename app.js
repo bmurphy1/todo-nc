31bce4150882cec82a6863f3c08fc89c390cd5e3
Brian Murphy <bmurphy1@mac.com>
2015-03-01 18:10:58 -0600

    Add todo:list command to App

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ TodoApp.addRegions({
     todoRegion: '#container',
     todoNewRegion: '#todo-new'
 });
+
+TodoApp.commands.setHandler('todo:list', function() {
+  window.todoListController = new TodoListController();
+});
