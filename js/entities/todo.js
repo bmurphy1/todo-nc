8e722bd56655aa33d569516662f9562b7b19d659
Brian Murphy <bmurphy1@mac.com>
2015-03-01 18:07:03 -0600

    Add Todo completion toggle checkbox functionality

--- js/entities/todo.js
+++ js/entities/todo.js
@@ -1,4 +1,14 @@
 TodoItem = Backbone.Model.extend({
+  toggleComplete: function() {
+    this.set('is_complete', !this.get('is_complete'));
+    Todo.updateTodo({
+      todoId: this.get('id'),
+      data: { description: this.get('description'), 
+              is_complete: this.get('is_complete') },
+      success: function(todo) { alert('todo update success!') },
+      error:   function(xhr)  { alert('todo update error!') }
+    });
+  }
 });
 
 TodoList = Backbone.Collection.extend({
--- js/todo/list/list_view.js
+++ js/todo/list/list_view.js
@@ -2,6 +2,17 @@ TodoItemView = Marionette.ItemView.extend({
   template: '#todo-item-view',
   tagName: 'tr',
   className: 'todo',
+  
+  ui: { checkbox: ':checkbox' },
+  events: { 'click @ui.checkbox': 'toggleComplete' },
+  
+  onRender: function() {
+    this.ui.checkbox.prop('checked', this.model.get('is_complete'));
+  },
+  
+  toggleComplete: function() { this.model.toggleComplete(); }
+});
+
 TodoListView = Marionette.CompositeView.extend({
   template: '#todo-list-view',
   tagName: 'table',
