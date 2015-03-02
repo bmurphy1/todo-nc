3bcac3d197c3fa141498dea76d0504d5053ef81c
Brian Murphy <bmurphy1@mac.com>
2015-03-01 18:52:06 -0600

    Create Todo Edit Controller handler

--- app.js
+++ app.js
@@ -8,3 +8,7 @@ TodoApp.addRegions({
 TodoApp.commands.setHandler('todo:list', function() {
   window.todoListController = new TodoListController();
 });
+
+TodoApp.commands.setHandler('todo:edit', function(model) {
+  window.todoEditController = new TodoEditController(model);
+});
