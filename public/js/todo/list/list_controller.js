85083854b2f9c7359354d625536a218786754868
Brian Murphy <bmurphy1@mac.com>
2015-03-05 15:36:27 -0600

    Whiespace removal

--- public/js/todo/list/list_controller.js
+++ public/js/todo/list/list_controller.js
@@ -1,13 +1,13 @@
 TodoListController = Marionette.Controller.extend({
   initialize: function() {
-    
+
     this.todoList = new TodoList();
     this.todoList.fetch();
-    
+
     this.todoListView = new TodoListView({collection: this.todoList});
     TodoApp.todoRegion.show(this.todoListView);
   },
-  
+
   onDestroy: function() {
     this.todoListView.destroy();
   }
--- public/js/todo/list/list_view.js
+++ public/js/todo/list/list_view.js
@@ -2,23 +2,23 @@ TodoItemView = Marionette.ItemView.extend({
   template: '#todo-item-view',
   tagName: 'tr',
   className: 'todo',
-  
+
   ui: { checkbox: ':checkbox',
             edit: '#edit' },
-  
+
   events: { 'click @ui.checkbox': 'toggleComplete',
                 'click @ui.edit': 'editTodo'},
-  
+
   onRender: function() {
     this.ui.checkbox.prop('checked', this.model.get('is_complete'));
   },
-  
+
   modelEvents: {'change:description': 'render'},
-  
+
   editTodo: function() {
     TodoApp.execute('todo:edit', this.model);
   },
-  
+
   toggleComplete: function() { this.model.toggleComplete(); },
 });
 
@@ -29,25 +29,25 @@ TodoListView = Marionette.CompositeView.extend({
   className: 'table',
   id: 'todos',
   childView: TodoItemView,
-  
+
   ui: {    'tbody': 'tbody',
       bulkComplete: '#bulk-complete' },
-  
+
   events: { 'click @ui.bulkComplete': 'bulkToggleComplete' },
 
-  collectionEvents: { 'add': 'render' },
-  
+  collectionEvents: { 'update': 'render' },
+
   onRender: function() {
     window.views = this.children;
     this.ui.tbody.sortable();
   },
-  
+
   bulkToggleComplete: function() {
     if (this.ui.bulkComplete.prop('checked')) {
       _.each(this.children._views, function(view) {view.ui.checkbox.prop('checked', true)});
       _.each(this.collection.models, function(model) {
         model.set('is_complete', true);
-        model.save();  
+        model.save();
       });
     } else {
       _.each(this.children._views, function(view) {view.ui.checkbox.prop('checked', false)});
